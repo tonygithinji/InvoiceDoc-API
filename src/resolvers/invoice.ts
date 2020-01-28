e55dc96c3646b85a4634f21f291b1f8f6919a62d
Anthony Githinji <tonygithinji94@gmail.com>
2020-01-28 17:45:46 +0300

    Added new invoice creation

--- src/resolvers/invoice.ts
+++ src/resolvers/invoice.ts
@@ -1,1 +1,20 @@
-export default {};
+export default {
+    // Queries
+    getInvoice: ({ id }: any, { models, user }: any) => models.Invoice.findOne({ where: { id, userId: user.id }, include: models.InvoiceItem }),
+    allInvoices: (args: any, { models, user }: any) => models.Invoice.findAll({ where: { userId: user.id } }),
+
+    // Mutations
+    createInvoice: async (args: any, { models, user }: any) => {
+        try {
+            const invoice = await models.Invoice.create({ ...args.invoice, userId: user.id });
+
+            for (const item of args.invoiceItems) {
+                await models.InvoiceItem.create({ ...item, invoiceId: invoice.id });
+            }
+            return true;
+        } catch (err) {
+            console.log("CREATE INVOICE", err);
+            return false;
+        }
+    }
+};
--- src/schemas/invoice.ts
+++ src/schemas/invoice.ts
@@ -7,5 +7,30 @@ export default `
         viewed: Int!
         createdAt: String!
         client: Client!
+        items: [InvoiceItem!]!
+    }
+
+    input InvoiceInput {
+        invoiceNumber: String!
+        amount: Float!
+        status: Int!
+        clientId: Int!
+    }
+
+    input InvoiceItemInput {
+        name: String!
+        qty: Float!
+        rate: Float!
+        amount: Float!
+    }
+
+    type Query {
+        getInvoice(id: Int!): Invoice!
+        allInvoices: [Invoice!]!
+
+    }
+
+    type Mutation {
+        createInvoice(invoice: InvoiceInput, invoiceItems: [InvoiceItemInput]): Boolean!
     }
 `;
